d2357f9313b32d0f484f7484347f593aa6f85928
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2021-03-16 15:22:29 +0100

    Add some initial testing
    Make project a Maven project and added some initial unit tests.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,23 @@ public class Main {
         }
     }
 
+    static boolean isSafe(int[][] board, int row, int column) {
+        //row
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == 1)
+                return false;
+        }
+        //column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] == 1)
+                return false;
+        }
+        //diagonal
+
+
+        return true;
+    }
+
     static int[][] generateBoard(int n) {
         int[][] board = new int[n][n];
 
@@ -21,6 +38,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        printBoard(generateBoard(8));
+        var board = generateBoard(8);
+
+        board[3][5] = 1;
+
+        printBoard(board);
     }
 }
